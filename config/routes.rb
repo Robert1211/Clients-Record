fd8909bf0838b366774a9c67b0deebd37aef4e6e
Robert1211 <maculewicz.au@gmail.com>
2018-10-11 15:38:41 +1100

    clean code pages-home,session-new,users-new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
-    resources :users, :only => [:new, :create, :index]
-    get '/customers/results' => 'customers#result'
-    resources :customers
-    resources :name, :only => [:new, :create]
+  resources :users, :only => [:new, :create, :index]
+  get '/customers/results' => 'customers#result'
+  resources :customers
+  resources :name, :only => [:new, :create]
   get '/login/' => 'session#new' # for login form
   post '/login' => 'session#create' # for a login
   delete '/login' => 'session#destroy' #  log out
