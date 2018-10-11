932133143132a423345db643638b828fee4023b3
Robert1211 <maculewicz.au@gmail.com>
2018-10-11 13:35:29 +1100

    clean code controller-session,users

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,21 +1,18 @@
 class SessionController < ApplicationController
   def new
   end
-   def create
-     # user = User.find_by :name => params[:name]
-     user = User.find_by :email => params[:email]
-     if user.present? && user.authenticate(params[:password])
-       session[:user_id] = user.id
-       redirect_to customers_path
+  def create
+    user = User.find_by :email => params[:email]
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to customers_path
     else
       flash[:error] = "Invalid email or password"
       redirect_to login_path
     end
   end
-
   def destroy
     session[:user_id] = nil
     redirect_to root_path
   end
-
 end
