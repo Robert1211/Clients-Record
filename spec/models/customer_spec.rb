1fa8d1c57003ea393dac7acc8b315c6e6f6721f0
Robert1211 <maculewicz.au@gmail.com>
2018-10-11 15:47:40 +1100

    clean code models-rspec(models-customer-spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   it 'requires a first name' do
-   customer = Customer.new(last_name: 'Biggles', email: 'robert@ga.co').save
-   expect(customer).to eq(false)
+    customer = Customer.new(last_name: 'Biggles', email: 'robert@ga.co').save
+    expect(customer).to eq(false)
   end
   it 'requires a last name' do
     customer = Customer.new(first_name: 'Robert', email: 'robert@ga.co').save
@@ -25,5 +25,5 @@ RSpec.describe Customer, type: :model do
     customer = Customer.new(first_name: 'Robert', last_name: 'Macul', email: 'robert@ga.co', phone: '1234', address: 'Sydney').save
     expect(customer).to eq(false)
   end
-  
+
 end
